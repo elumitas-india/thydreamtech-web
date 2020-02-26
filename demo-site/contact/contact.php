43e0654ba03f166d8319b0ddd47f777979200c1f
senthilmurugangl <?senthil.murugan@thydreamtech.com>
2020-02-26 16:38:14 +0530

    Contact form testing.

--- demo-site/contact/contact.php
+++ demo-site/contact/contact.php
@@ -4,10 +4,10 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'Demo contact form <senthil.murugan@thydreamtech.com>';
+$from = 'senthil.murugan@thydreamtech.com';
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'Demo contact form <senthil.murugan@thydreamtech.com>';
+$sendTo = 'senthil.murugan@thydreamtech.com';
 
 // subject of the email
 $subject = 'New message from contact form';
